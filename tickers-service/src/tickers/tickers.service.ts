d176d928975ddfe6dc0ef6ead5c71ee2cd3ee64b
fresemin <fresemin@gmail.com>
2024-02-17 00:45:40 +0300

    feat(ticker-service): get all tickers from api

--- tickers-service/src/tickers/tickers.service.ts
+++ tickers-service/src/tickers/tickers.service.ts
@@ -6,6 +6,13 @@ import {
   DEFAULT_CRON_SYNC_TICKERS_NAME,
   DEFAULT_CRON_SYNC_TICKERS_TIME,
 } from 'src/constants';
+import {
+  AllTickers,
+  AllTickersResponse,
+  Ticker,
+  TickerForConvertFields,
+} from 'src/models';
+import { convertFieldsToNumber, convertToFloatNumber } from 'src/utils';
 
 @Injectable()
 export class TickersService {
@@ -38,8 +45,52 @@ export class TickersService {
     this.tickersCron = this.schedulerRegistry.getCronJob(this.syncCronJobName);
   }
 
-  private syncTickers(): void {
-    this.logger.log('SyncTickers at time', Date.now());
+  private async getAllTickers(): Promise<AllTickers<number>> {
+    // TODO: add api to constants and env
+    const apiResponse: Response = await fetch(
+      'https://api.kucoin.com/api/v1/market/allTickers',
+    );
+
+    const allTickersResponse: AllTickersResponse = await apiResponse.json();
+
+    if (allTickersResponse.data) {
+      const convertedTickers: Ticker<number>[] =
+        allTickersResponse.data.ticker.map((ticker): Ticker<number> => {
+          const { symbol, symbolName, ...fieldsToNumber } = ticker;
+          return {
+            symbol,
+            symbolName,
+            ...convertFieldsToNumber<TickerForConvertFields<number>>(
+              fieldsToNumber,
+            ),
+          };
+        });
+
+      return {
+        time: convertToFloatNumber(allTickersResponse.data.time),
+        ticker: convertedTickers,
+      };
+    } else {
+      // TODO: throw custom error
+      throw new Error('Get All Tickers Error');
+    }
+  }
+
+  private async syncTickers(): Promise<void> {
+    try {
+      this.logger.log(
+        `Cron Job: ${this.syncCronJobName} started: ${Date.now()}`,
+      );
+
+      const allTickers: AllTickers<number> = await this.getAllTickers();
+
+      this.logger.log(
+        `Cron Job: ${this.syncCronJobName} finished: ${Date.now()}`,
+      );
+    } catch (error) {
+      // TODO: improve error handling
+      this.logger.error(error.message);
+    }
   }
 
   startSyncTickersCron(): void {
