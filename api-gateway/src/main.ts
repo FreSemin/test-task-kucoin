bc31825b8af6ddd52538e54a3fa188fe63fb07dd
fresemin <fresemin@gmail.com>
2024-02-18 17:47:38 +0300

    feat(api-gateway): api prefix env

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix(process.env.APP_API_PREFIX || '');
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(Number(process.env.APP_API_PORT) || 3000);
