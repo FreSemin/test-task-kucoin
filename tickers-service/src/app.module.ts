8b0d5f3634239caad517f63c08a8f1ed4111b226
fresemin <fresemin@gmail.com>
2024-02-18 05:24:19 +0300

    ref(ticker-service): delete unused app service controller

--- tickers-service/src/app.module.ts
+++ tickers-service/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TickerModule } from './modules/tickers/ticker.module';
 import { ScheduleModule } from '@nestjs/schedule';
@@ -11,7 +9,5 @@ import { ScheduleModule } from '@nestjs/schedule';
     ScheduleModule.forRoot(),
     TickerModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
