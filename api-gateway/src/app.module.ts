0537a7aecc766a998c2a4743822dbb77ce42fb9c
fresemin <fresemin@gmail.com>
2024-02-19 02:50:51 +0300

    feat(api-gateway): user module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SymbolModule } from './modules/symbol/symbol.module';
 import { TickerModule } from './modules/ticker/ticker.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SymbolModule, TickerModule],
+  imports: [ConfigModule.forRoot(), SymbolModule, TickerModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
