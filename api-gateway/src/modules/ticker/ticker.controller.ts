ab0f81071dfb82ef7b39c894ada997b6bb88a1d1
fresemin <fresemin@gmail.com>
2024-02-18 20:21:37 +0300

    feat(api-gateway): ticker get all

--- api-gateway/src/modules/ticker/ticker.controller.ts
+++ api-gateway/src/modules/ticker/ticker.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TickerService } from './ticker.service';
+import { Ticker } from '@prisma/client';
 
 @Controller('ticker')
 export class TickerController {
   constructor(private readonly tickerService: TickerService) {}
+
+  @Get('all')
+  async findAll(): Promise<Ticker[]> {
+    return await this.tickerService.findAll();
+  }
 }
--- api-gateway/src/modules/ticker/ticker.service.ts
+++ api-gateway/src/modules/ticker/ticker.service.ts
@@ -1,5 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Ticker } from '@prisma/client';
+import { PRISMA_SORT_ASC } from 'src/constants';
 
 @Injectable()
-export class TickerService extends PrismaClient {}
+export class TickerService extends PrismaClient {
+  async findAll(): Promise<Ticker[]> {
+    return await this.ticker.findMany({
+      include: {
+        symbol: true,
+      },
+      orderBy: {
+        symbolId: PRISMA_SORT_ASC,
+      },
+    });
+  }
+}
