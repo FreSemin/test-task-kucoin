53ff24a6ab048486d4ceeee9929e591187da5a85
fresemin <fresemin@gmail.com>
2024-02-19 01:39:55 +0300

    feat(api-gateway): period not valid format constant

--- api-gateway/src/constants/index.ts
+++ api-gateway/src/constants/index.ts
@@ -5,3 +5,6 @@ export const SYMBOL_NOT_FOUND_EXCEPTION = (symbol: string | number) =>
   `Symbol: ${symbol} was not found!`;
 export const TICKER_NOT_FOUND_EXCEPTION = (symbol: string | number) =>
   `Ticker: ${symbol} was not found!`;
+
+export const PERIOD_NOT_VALID_FORMAT = (propertyName: string) =>
+  `${propertyName} should be in format: YYYY-MM-DDTHH:mm`;
