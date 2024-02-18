546d91feb9c0aacb01cc34df1844ced4e34f65d3
fresemin <fresemin@gmail.com>
2024-02-18 20:22:01 +0300

    fix(api-gateway): parse bigint to json

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// FIX: TypeError: Do not know how to serialize a BigInt
+import './utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
