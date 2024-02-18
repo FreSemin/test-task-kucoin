63ac3ebbf3b08a3e09ee29ff710c606101c0b51f
fresemin <fresemin@gmail.com>
2024-02-18 03:06:49 +0300

    feat(ticker-service): prisma symbol service

---
+++ tickers-service/src/modules/prisma/prisma-symbol.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { TickerSymbol, TickerSymbolData } from 'src/models';
+
+@Injectable()
+export class PrismaSymbolService extends PrismaClient {
+  // TODO: add return type
+  async syncSymbols(symbolsData: TickerSymbolData[]): Promise<TickerSymbol[]> {
+    return await Promise.allSettled([
+      ...symbolsData.map((symbolData) => {
+        return new Promise(async (resolve, reject) => {
+          try {
+            resolve(
+              await this.symbol.upsert({
+                where: {
+                  symbol: symbolData.symbol,
+                },
+                update: {
+                  symbolName: symbolData.symbolName,
+                },
+                create: {
+                  ...symbolData,
+                },
+              }),
+            );
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }),
+    ])
+      .then((symbols: Array<PromiseSettledResult<TickerSymbol>>) => {
+        return symbols
+          .filter((symbol: PromiseSettledResult<TickerSymbol>) => {
+            // TODO: add status to constants
+            return symbol.status === 'fulfilled';
+          })
+          .map((symbol: PromiseFulfilledResult<TickerSymbol>) => symbol.value);
+      })
+      .catch((error) => {
+        // TODO: create custom error
+        throw error;
+      });
+  }
+}
--- tickers-service/src/modules/prisma/prisma.module.ts
+++ tickers-service/src/modules/prisma/prisma.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PrismaTickersService } from './prisma-tickers.service';
 import { PrismaTickersHistoryService } from './prisma-tickers-history.service';
+import { PrismaSymbolService } from './prisma-symbol.service';
 
 @Module({
-  providers: [PrismaTickersService, PrismaTickersHistoryService],
-  exports: [PrismaTickersService, PrismaTickersHistoryService],
+  providers: [
+    PrismaTickersService,
+    PrismaTickersHistoryService,
+    PrismaSymbolService,
+  ],
+  exports: [
+    PrismaTickersService,
+    PrismaTickersHistoryService,
+    PrismaSymbolService,
+  ],
 })
 export class PrismaModule {}
