e2d0dfe720a043280dfa02f7aa307d237d622e51
fresemin <fresemin@gmail.com>
2024-02-18 04:57:43 +0300

    ref(ticker-service): prisma init tables migration

--- tickers-service/prisma/migrations/20240217110714_tickers_types/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Tickers" ALTER COLUMN "updatedTime" SET DATA TYPE BIGINT;
-
--- AlterTable
-ALTER TABLE "TickersHistory" ALTER COLUMN "updatedTime" SET DATA TYPE BIGINT;
--- tickers-service/prisma/migrations/20240218015700_init/migration.sql
+++ tickers-service/prisma/migrations/20240218015700_init/migration.sql
@@ -1,50 +1,16 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Ticker` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `TickerHistory` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "TickerHistory" DROP CONSTRAINT "TickerHistory_tickerId_fkey";
-
--- DropTable
-DROP TABLE "Ticker";
-
--- DropTable
-DROP TABLE "TickerHistory";
-
 -- CreateTable
-CREATE TABLE "Tickers" (
+CREATE TABLE "Symbol" (
     "id" SERIAL NOT NULL,
     "symbol" TEXT NOT NULL,
     "symbolName" TEXT NOT NULL,
-    "buy" DOUBLE PRECISION NOT NULL,
-    "sell" DOUBLE PRECISION NOT NULL,
-    "bestBidSize" DOUBLE PRECISION NOT NULL,
-    "bestAskSize" DOUBLE PRECISION NOT NULL,
-    "changeRate" DOUBLE PRECISION NOT NULL,
-    "changePrice" DOUBLE PRECISION NOT NULL,
-    "high" DOUBLE PRECISION NOT NULL,
-    "low" DOUBLE PRECISION NOT NULL,
-    "vol" DOUBLE PRECISION NOT NULL,
-    "volValue" DOUBLE PRECISION NOT NULL,
-    "last" DOUBLE PRECISION NOT NULL,
-    "averagePrice" DOUBLE PRECISION NOT NULL,
-    "takerFeeRate" DOUBLE PRECISION NOT NULL,
-    "makerFeeRate" DOUBLE PRECISION NOT NULL,
-    "takerCoefficient" DOUBLE PRECISION NOT NULL,
-    "makerCoefficient" DOUBLE PRECISION NOT NULL,
-    "updatedTime" INTEGER NOT NULL,
 
-    CONSTRAINT "Tickers_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Symbol_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "TickersHistory" (
+CREATE TABLE "Ticker" (
     "id" SERIAL NOT NULL,
-    "symbol" TEXT NOT NULL,
-    "symbolName" TEXT NOT NULL,
+    "symbolId" INTEGER NOT NULL,
     "buy" DOUBLE PRECISION NOT NULL,
     "sell" DOUBLE PRECISION NOT NULL,
     "bestBidSize" DOUBLE PRECISION NOT NULL,
@@ -61,11 +27,48 @@ CREATE TABLE "TickersHistory" (
     "makerFeeRate" DOUBLE PRECISION NOT NULL,
     "takerCoefficient" DOUBLE PRECISION NOT NULL,
     "makerCoefficient" DOUBLE PRECISION NOT NULL,
-    "updatedTime" INTEGER NOT NULL,
+    "updatedTime" BIGINT NOT NULL,
+
+    CONSTRAINT "Ticker_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TickerHistory" (
+    "id" SERIAL NOT NULL,
+    "symbolId" INTEGER NOT NULL,
+    "buy" DOUBLE PRECISION NOT NULL,
+    "sell" DOUBLE PRECISION NOT NULL,
+    "bestBidSize" DOUBLE PRECISION NOT NULL,
+    "bestAskSize" DOUBLE PRECISION NOT NULL,
+    "changeRate" DOUBLE PRECISION NOT NULL,
+    "changePrice" DOUBLE PRECISION NOT NULL,
+    "high" DOUBLE PRECISION NOT NULL,
+    "low" DOUBLE PRECISION NOT NULL,
+    "vol" DOUBLE PRECISION NOT NULL,
+    "volValue" DOUBLE PRECISION NOT NULL,
+    "last" DOUBLE PRECISION NOT NULL,
+    "averagePrice" DOUBLE PRECISION NOT NULL,
+    "takerFeeRate" DOUBLE PRECISION NOT NULL,
+    "makerFeeRate" DOUBLE PRECISION NOT NULL,
+    "takerCoefficient" DOUBLE PRECISION NOT NULL,
+    "makerCoefficient" DOUBLE PRECISION NOT NULL,
+    "updatedTime" BIGINT NOT NULL,
     "tickerId" INTEGER NOT NULL,
 
-    CONSTRAINT "TickersHistory_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "TickerHistory_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Symbol_symbol_key" ON "Symbol"("symbol");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Ticker_symbolId_key" ON "Ticker"("symbolId");
+
 -- AddForeignKey
-ALTER TABLE "TickersHistory" ADD CONSTRAINT "TickersHistory_tickerId_fkey" FOREIGN KEY ("tickerId") REFERENCES "Tickers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Ticker" ADD CONSTRAINT "Ticker_symbolId_fkey" FOREIGN KEY ("symbolId") REFERENCES "Symbol"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TickerHistory" ADD CONSTRAINT "TickerHistory_tickerId_fkey" FOREIGN KEY ("tickerId") REFERENCES "Ticker"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TickerHistory" ADD CONSTRAINT "TickerHistory_symbolId_fkey" FOREIGN KEY ("symbolId") REFERENCES "Symbol"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
