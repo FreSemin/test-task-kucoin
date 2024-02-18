8c48f746d0a65d08b150bcd9f76eefc352b54ab9
fresemin <fresemin@gmail.com>
2024-02-18 16:50:09 +0300

    feat(api-gateway): symbol find all

--- api-gateway/src/modules/symbol/symbol.controller.ts
+++ api-gateway/src/modules/symbol/symbol.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { SymbolService } from './symbol.service';
+import { TickerSymbol } from 'src/models';
 
 @Controller('symbol')
 export class SymbolController {
   constructor(private readonly symbolService: SymbolService) {}
+
+  @Get('all')
+  async findAll(): Promise<TickerSymbol[]> {
+    return await this.symbolService.findAll();
+  }
 }
--- api-gateway/src/modules/symbol/symbol.service.ts
+++ api-gateway/src/modules/symbol/symbol.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { SORT_ASC } from 'src/constants';
+import { TickerSymbol } from 'src/models';
 
 @Injectable()
-export class SymbolService {}
+export class SymbolService extends PrismaClient {
+  async findAll(): Promise<TickerSymbol[]> {
+    return await this.symbol.findMany({
+      orderBy: [
+        {
+          id: SORT_ASC,
+        },
+      ],
+    });
+  }
+}
