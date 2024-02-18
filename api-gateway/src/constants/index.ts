f837fa8c9295f6b7eaf91a5f7b394313931ded39
fresemin <fresemin@gmail.com>
2024-02-18 17:51:09 +0300

    feat(api-gateway): symbol not found exception

--- api-gateway/src/constants/index.ts
+++ api-gateway/src/constants/index.ts
@@ -1,3 +1,5 @@
 export const PRISMA_SORT_ASC = 'asc';
 export const PRISMA_SORT_DESC = 'desc';
 
+export const SYMBOL_NOT_FOUND_EXCEPTION = (symbol: string | number) =>
+  `Symbol: ${symbol} was not found!`;
