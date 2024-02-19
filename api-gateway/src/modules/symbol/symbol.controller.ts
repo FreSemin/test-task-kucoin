472ef4630149a6e9159f5a231cac7d0f9cc7bbd5
fresemin <fresemin@gmail.com>
2024-02-19 06:21:03 +0300

    feat(api-gateway): use auth guard symbol

--- api-gateway/src/modules/symbol/symbol.controller.ts
+++ api-gateway/src/modules/symbol/symbol.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { SymbolService } from './symbol.service';
 import { TickerSymbol } from 'src/models';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('symbol')
 export class SymbolController {
   constructor(private readonly symbolService: SymbolService) {}
 
+  @UseGuards(AuthGuard)
   @Get('all')
   async findAll(): Promise<TickerSymbol[]> {
     return await this.symbolService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':symbol')
   async findOneById(@Param('symbol') symbol: string): Promise<TickerSymbol> {
     return await this.symbolService.findOneBySymbol(symbol);
