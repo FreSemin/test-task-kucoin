fa0350c4efd2425466cbeda68b82b2e0fe79fc51
fresemin <fresemin@gmail.com>
2024-02-19 06:21:11 +0300

    feat(api-gateway): use auth guard ticker

--- api-gateway/src/modules/ticker/ticker.controller.ts
+++ api-gateway/src/modules/ticker/ticker.controller.ts
@@ -1,21 +1,25 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { TickerService } from './ticker.service';
 import { Period, Ticker, TickerHistory } from 'src/models';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('ticker')
 export class TickerController {
   constructor(private readonly tickerService: TickerService) {}
 
+  @UseGuards(AuthGuard)
   @Get('all')
   async findAll(): Promise<Ticker[]> {
     return await this.tickerService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':symbol')
   async findOneBySymbol(@Param('symbol') symbol: string): Promise<Ticker> {
     return await this.tickerService.findOneBySymbol(symbol);
   }
 
+  @UseGuards(AuthGuard)
   @Get('/history/:symbol')
   async findHistoryBySymbol(
     @Param('symbol') symbol: string,
