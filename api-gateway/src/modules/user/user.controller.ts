8101886446e9bb532b7a88f9a09955c650c7f0c8
fresemin <fresemin@gmail.com>
2024-02-19 04:23:31 +0300

    feat(api-gateway): create user

--- api-gateway/src/modules/user/user.controller.ts
+++ api-gateway/src/modules/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './input/create-user.dto';
+import { User } from 'src/models';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // TODO: hash password
+  // TODO: hide password from response
+  // TODO: Send created token
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.userService.create(createUserDto);
+  }
+}
--- api-gateway/src/modules/user/user.service.ts
+++ api-gateway/src/modules/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { CreateUserDto } from './input/create-user.dto';
+import { User } from 'src/models';
 
 @Injectable()
-export class UserService {}
+export class UserService extends PrismaClient {
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { retypedPassword, ...userData } = createUserDto;
+
+    return await this.user.create({
+      data: userData,
+    });
+  }
+}
